78007fa22329bc5c092ec4407a17677278e198b1
sombra61 <vitor.jose.rocha24@gmail.com>
2026-07-01 08:48:53 -0300

    eai

--- script.js
+++ script.js
@@ -12,29 +12,50 @@ document.querySelectorAll(".card").forEach(card => {
 
         const img = card.querySelector(".verso img");
 
-        if(indice === -1){
+        if (indice === -1) {
 
             indice = 0;
             img.src = imagens[indice];
-
             card.classList.add("virado");
-
             return;
+
         }
 
         indice++;
 
-        if(indice < imagens.length){
+        if (indice < imagens.length) {
 
             img.src = imagens[indice];
 
-        }else{
+        } else {
 
             indice = -1;
-
             card.classList.remove("virado");
+
         }
 
+    });
+
+});
+
+const tooltip = document.createElement("div");
+tooltip.className = "tooltip";
+document.body.appendChild(tooltip);
+
+document.querySelectorAll(".links a").forEach(link => {
+
+    link.addEventListener("mouseenter", () => {
+        tooltip.textContent = link.dataset.info;
+        tooltip.classList.add("mostrar");
+    });
+
+    link.addEventListener("mousemove", e => {
+        tooltip.style.left = `${e.pageX + 15}px`;
+        tooltip.style.top = `${e.pageY + 15}px`;
+    });
+
+    link.addEventListener("mouseleave", () => {
+        tooltip.classList.remove("mostrar");
     });
 
 });
