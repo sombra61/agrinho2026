97a453daa33fcb1b888445fdbc1525ad15b3287b
sombra61 <vitor.jose.rocha24@gmail.com>
2026-07-01 08:53:27 -0300

    ba

--- script.js
+++ script.js
@@ -13,25 +13,19 @@ document.querySelectorAll(".card").forEach(card => {
         const img = card.querySelector(".verso img");
 
         if (indice === -1) {
-
             indice = 0;
             img.src = imagens[indice];
             card.classList.add("virado");
             return;
-
         }
 
         indice++;
 
         if (indice < imagens.length) {
-
             img.src = imagens[indice];
-
         } else {
-
             indice = -1;
             card.classList.remove("virado");
-
         }
 
     });
@@ -44,34 +38,35 @@ document.body.appendChild(tooltip);
 
 document.querySelectorAll(".links a").forEach(link => {
 
+    link.addEventListener("mouseenter", () => {
+        tooltip.textContent = link.dataset.info;
+        tooltip.classList.add("mostrar");
+    });
+
     link.addEventListener("mousemove", e => {
 
-    const margem = 20;
+        const margem = 20;
 
-    let x = e.clientX + margem;
-    let y = e.clientY + margem;
+        let x = e.clientX + margem;
+        let y = e.clientY + margem;
 
-    const largura = tooltip.offsetWidth;
-    const altura = tooltip.offsetHeight;
+        const largura = tooltip.offsetWidth;
+        const altura = tooltip.offsetHeight;
 
-    if (x + largura > window.innerWidth) {
-        x = e.clientX - largura - margem;
-    }
+        if (x + largura > window.innerWidth) {
+            x = e.clientX - largura - margem;
+        }
 
-    if (y + altura > window.innerHeight) {
-        y = e.clientY - altura - margem;
-    }
+        if (y + altura > window.innerHeight) {
+            y = e.clientY - altura - margem;
+        }
 
-    if (x < margem) x = margem;
-    if (y < margem) y = margem;
+        if (x < margem) x = margem;
+        if (y < margem) y = margem;
 
-    tooltip.style.left = `${x}px`;
-    tooltip.style.top = `${y}px`;
+        tooltip.style.left = `${x}px`;
+        tooltip.style.top = `${y}px`;
 
-});
-    link.addEventListener("mousemove", e => {
-        tooltip.style.left = `${e.pageX + 15}px`;
-        tooltip.style.top = `${e.pageY + 15}px`;
     });
 
     link.addEventListener("mouseleave", () => {
